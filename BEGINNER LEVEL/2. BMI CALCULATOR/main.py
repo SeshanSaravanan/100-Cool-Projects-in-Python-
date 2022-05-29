79e1ef1f1bb34a78fc48485b41c934b97d114512
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-05-29 17:04:58 +0530

    Create main.py

---
+++ BEGINNER LEVEL/2. BMI CALCULATOR/main.py
@@ -0,0 +1,14 @@
+ 
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+bmi = float(weight) / float(height) ** 2
+print(bmi)
+
+#for bmi value as integers 
+bmi_as_int = int(bmi)
+print(bmi_as_int)
